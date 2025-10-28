a9e0bd0df678f58818d57b7f7edb43af033ee020
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-10-29 01:16:51 +0300

    refactor: move lucky_random.py to src directory

---
+++ src/lucky_random.py
@@ -0,0 +1,15 @@
+import requests
+class Client():
+	def __init__(self):
+		self.api="https://lucky-random.ru/modules"
+		self.headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36","x-requested-with": "XMLHttpRequest"}
+	def random_nummer(self,unique:int=1,count:int=1):
+		return requests.get(f"{self.api}/nrand/api.php?count={count}&min=1&max=1000&unique={unique}",headers=self.headers).text
+	def random_nummer_2(self,unique:int=1,count:int=1):
+		return requests.get(f"{self.api}/nrand/api2.php?count={count}&min=1&max=1000&unique={unique}",headers=self.headers).text
+	def random_steps(self,step:int=1):
+		return requests.get(f"{self.api}/nrandsteps/api.php?step={step}&max=100",headers=self.headers).text
+	def random_fact(self):
+		return requests.get(f"{self.api}/frand/api.php",headers=self.headers).text
+	def random_wisdom(self):
+		return requests.get(f"{self.api}/mrand/api.php",headers=self.headers).text
